07fe22f5dd590ea03c300559b717b835533f6ba5
cpasquali <conipasquali2@gmail.com>
2025-08-26 18:12:15 -0300

    arreglos visuales en mobile

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -26,7 +26,7 @@ export const PostList = () => {
         <section className="mt-10 w-full flex flex-col items-center justify-center sm:w-[45%] gap-4 pt-2">
           {Array(9)
             .fill(null)
-            .map((e, index) => (
+            .map((_, index) => (
               <PostCardSkeleton key={index} />
             ))}
         </section>
@@ -35,7 +35,7 @@ export const PostList = () => {
   }
 
   return (
-    <section className="mt-[-36px] sm:ml-[-160px] sm:mt-10 w-full flex flex-col items-center justify-center">
+    <section className="mt-[-36px] sm:ml-[-160px] sm:mt-10 w-full flex flex-col items-center justify-center bg-gray-200 sm:bg-white">
       <button
         onClick={handleOpenModal}
         className="cursor-pointer hidden sm:flex items-center justify-center gap-2 h-10 bg-blue-600 text-white hover:bg-blue-700 font-semibold transition w-80 text-lg"
@@ -43,7 +43,7 @@ export const PostList = () => {
         Subir post <ion-icon name="duplicate-outline"></ion-icon>
       </button>
 
-      <section className="mt-10 w-full flex flex-col items-center justify-center sm:w-[45%] gap-4 pt-2">
+      <section className="mt-10 w-full flex flex-col items-center justify-center sm:w-[45%] gap-2 sm:gap-4 pt-2">
         {postList &&
           postList.length > 0 &&
           postList.map((p) => <PostCard key={p.id} post={p} />)}
